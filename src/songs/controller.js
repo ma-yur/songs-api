ed08b143a64372cdbbe57d72678c118ec08dd27e
mayur kumar <kumarmayur797@gmail.com>
2021-10-26 16:59:52 +0530

    add post request to songs

--- src/songs/controller.js
+++ src/songs/controller.js
@@ -10,11 +10,24 @@ const getSongById = (req, res) => {
 	const id = parseInt(req.params.id);
 	pool.query(`SELECT * FROM songs Where id=${id}`, (err, result) => {
 		if (err) throw err.message;
+		if (!result.rowCount)
+			res.status(404).send("no resource found with given id");
 		res.status(200).json(result.rows);
 	});
+};
+const addSong = (req, res) => {
+	const { song, artist, year } = req.body;
+	pool.query(
+		`INSERT INTO songs(song,artist,year) VALUES('${song}','${artist}',${year})`,
+		(err, result) => {
+			if (err) throw err;
+			res.status(202).send("new song added");
+		}
+	);
 };
 
 module.exports = {
 	getSongs,
 	getSongById,
+	addSong,
 };
--- src/songs/routes.js
+++ src/songs/routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
-const { getSongs, getSongById } = require("./controller");
+const { getSongs, getSongById, addSong } = require("./controller");
 
 const router = Router();
 
 router.get("/", getSongs);
 router.get("/:id", getSongById);
+
+router.post("/", addSong);
 
 module.exports = router;
